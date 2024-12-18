23fc92fa357186b9275a174570107f9f11778bfc
Falalu Ibrahim Lawan <flawan@aust.edu.ng>
2024-12-18 09:55:56 +0100

    added other stuffs to functions

---
+++ 02_Python-Lessons/reply.py
@@ -0,0 +1,5 @@
+reply = 0
+while reply != 4:
+    
+    reply = int(input("Reply with: \n1. to load air time\n2. to buy data\n3. to transfer data\n4. to quit\nEnter your choice [1-4] :"))
+    print(f"you've chosen {reply}")
